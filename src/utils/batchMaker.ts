4e1f66c15bc13601c8571d6e896f0f729e819d04
Maaz <mdmaazi2907@gmail.com>
2024-02-17 12:43:29 +0530

    optimize and worked on storing timestamps

--- src/utils/batchMaker.ts
+++ src/utils/batchMaker.ts
@@ -1,9 +1,12 @@
 import { Log } from "./applyRegEx";
 
-export function batchMaker(payload: Log[], batchSize: number): Log[][] {
+export function batchMaker(unPushedLogs: Log[], batchSize: number): Log[][] {
+  if (unPushedLogs.length <= 0) {
+    return [];
+  }
   let batchs: Log[][] = [];
-  for (let i = 0; i < payload.length - 1; i += batchSize) {
-    const batch = payload.slice(i, i + batchSize);
+  for (let i = 0; i < unPushedLogs.length - 1; i += batchSize) {
+    const batch = unPushedLogs.slice(i, i + batchSize);
     if (batch) {
       batchs.push(batch);
     }

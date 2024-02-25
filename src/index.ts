28677be5e5b6de1fcaf80a484ccb4afaaf758ccc
Maaz <mdmaazi2907@gmail.com>
2024-02-25 12:48:40 +0530

    trying to optimize the solution

--- src/index.ts
+++ src/index.ts
@@ -18,28 +18,32 @@ let oneDayAgo: Date = new Date(now - 24 * 60 * 60 * 1000);
 let dirName = process.env.DIR_NAME;
 
 // fetching all files path from provided directory
+console.log("fetching all files path from provided directory.");
 let filePath: string[] = fetchFilesPath(dirName);
 
-// array for storing array of log objects
+// array of storing array of log objects
 let logsPerFile: Log[][] = [];
 
+console.log("Reading logs from each path and creating an object out of it.");
 // fetching array of log object from each file path so every file will return array of log.
 filePath.forEach((file) => {
-  // reading file and returning array of log objects
-  let arrayOfObjects: Log[] = splitLogString(file);
-  // pushing array of log objects into this array
-  logsPerFile.push(arrayOfObjects);
+  if (file) {
+    // reading file and returning array of log objects
+    let arrayOfObjects: Log[] = splitLogString(file);
+    // pushing array of log objects into this array
+    logsPerFile.push(arrayOfObjects);
+  }
 });
 
-let unFilteredLogs: Log[] = [];
-
-// looping through each log object and checking if they are one day old if so then pushing them to payLoad
+let validLogs: Log[] = [];
+// looping through each log object and checking if they are one day old if so then pushing them to validLogs
+console.log("Checking that each log is one day old...");
 for (let arrayOfLogs of logsPerFile) {
   for (let i = 0; i < arrayOfLogs.length - 1; i++) {
     let log: Log = arrayOfLogs[i];
     let timestamp = new Date(log.timestamp);
     if (timestamp >= oneDayAgo && timestamp < now) {
-      unFilteredLogs.push(log);
+      validLogs.push(log);
     }
   }
 }
@@ -47,19 +51,29 @@ for (let arrayOfLogs of logsPerFile) {
 // retriving the timestamp of pushed logs
 let timestamps = getTimeStamps();
 
+console.log("Checking if we already pushed that log to dynatrace...");
 // to store unPushed logs
-let unPushedLogs: Log[] = checkForDublicate(timestamps, unFilteredLogs);
+let unPushedLogs: Log[] = checkForDublicate(timestamps, validLogs);
 
 let batchSize = 17000;
-// spliting the logs array into multiple log arrays
-let arrayOfBatchs: Log[][] = batchMaker(unPushedLogs, batchSize);
+console.log(
+  `Creating batched of logs if size of unPushed logs is greater than ${batchSize}`
+);
+if (unPushedLogs.length > batchSize) {
+  let arrayOfBatchs: Log[][] = batchMaker(unPushedLogs, batchSize);
 
-if (arrayOfBatchs.length) {
-  arrayOfBatchs.forEach((batch) => {
-    pushToDynatrace(batch);
-  });
+  if (arrayOfBatchs.length) {
+    arrayOfBatchs.forEach((batch) => {
+      pushToDynatrace(batch);
+    });
+  }
+  console.log("total logs", validLogs.length);
+  console.log("total batchs Pushed:", arrayOfBatchs.length);
+} else {
+  pushToDynatrace(unPushedLogs);
+  console.log("total logs", validLogs.length);
+  console.log("total batchs Pushed:", unPushedLogs.length);
 }
-console.log("total logs", unFilteredLogs.length);
-console.log("total batchs Pushed:", arrayOfBatchs.length);
-console.log(`logs per batch <= ${batchSize} `);
+// spliting the logs array into multiple log arrays
+console.log(`logs per batch ${batchSize} or less `);
 console.log("End 🍀");
